adcdbe6eea1d51f8302349477778b377d61e7979
Pratik Jha <pratikpro1999@gmail.com>
2022-07-05 21:35:15 +0530

    use brace for Ace theme

--- client/src/Acing.js
+++ client/src/Acing.js
@@ -2,25 +2,46 @@ import React from "react";
 import { render } from "react-dom";
 import AceEditor from "react-ace";
 
-import "ace-builds/src-noconflict/mode-java";
-import "ace-builds/src-noconflict/theme-github";
-import "ace-builds/src-noconflict/ext-language_tools";
-
+var ace = require('brace');
+import 'brace/mode/javascript';
+import 'brace/theme/github';
+import "brace/theme/monokai"
 
 function onChange(newValue) {
   console.log("change", newValue);
 }
 
-
+const editorStyle = {
+  border: '1px solid lightgray',
+};
 
 render(
   <AceEditor
-    mode="java"
-    theme="github"
-    onChange={onChange}
-    name="ace"
-    editorProps={{ $blockScrolling: true }}
-  />,
+  style={editorStyle}
+  readOnly={false}
+  
+  width="100%"
+  height="200px"
+  mode="javascript"
+  theme="github"
+  name="aceCodeEditor"
+  // onLoad={this.onLoad}
+  fontSize={14}
+  showPrintMargin
+  showGutter
+  highlightActiveLine
+  
+  editorProps={{
+    $blockScrolling: true,
+    enableBasicAutocompletion: true,
+    enableLiveAutocompletion: true,
+    enableSnippets: true,
+  }}
+  setOptions={{
+    showLineNumbers: true,
+    tabSize: 2,
+  }}
+/>,
   document.getElementById("ace")
 );
 
