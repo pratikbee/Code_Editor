10c1721602e814920bcff4cf35706e4082910524
Pratik Jha <pratikpro1999@gmail.com>
2022-07-16 22:55:41 +0530

    all client side component set

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,14 @@ import "./App.scss";
 
 import { Form, FormGroup, Col } from "react-bootstrap";
 
-import Output from "./Output";
-import StatusImage from "./StatusImage";
+
+
 
 const App = () => {
   return (
-    <Form>
-    <FormGroup>
-      <Col sm={12}>
-        <StatusImage message="hah"></StatusImage>
-      </Col>
-    </FormGroup>
-    </Form>
+    <>
+    
+    </>
   );
 };
 
